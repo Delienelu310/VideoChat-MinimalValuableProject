509335ef74a1d0a4e7163ed486eff608ddf586fa
Delienelu310 <seredoha.nets@gmail.com>
2023-10-14 12:27:44 +0200

    project initialization

---
+++ backend/videochat-mvp/src/main/java/com/perfonalprojects/videochatmvp/VideochatMvpApplication.java
@@ -0,0 +1,13 @@
+package com.perfonalprojects.videochatmvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideochatMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideochatMvpApplication.class, args);
+	}
+
+}
---
+++ backend/videochat-mvp/src/test/java/com/perfonalprojects/videochatmvp/VideochatMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfonalprojects.videochatmvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideochatMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
